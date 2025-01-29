8bae5d100d8c74d826548b017acb5fb24d451bb9
notbaopham <113058628+notbaopham@users.noreply.github.com>
2025-01-28 21:31:44 -0800

    Added InputManager.cs, fixed naming conventions

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 
-public class BallCollider : MonoBehaviour
+public class BallController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Rigidbody sphereRigidbody;
-    public float ballSpeed = 2f;
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] float ballSpeed = 2f;
+
+    public void MoveBall(Vector2 input)
+    {
+        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+    }
     void Start()
     {
         Debug.Log("Calling the Start method");
